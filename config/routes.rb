ee799371da8245a682902b498fb1d9d2334465dc
Kevin Kroos <kevinkroos@gmail.com>
2022-07-10 09:09:23 +0200

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
+  end
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "pages#home"
+end
